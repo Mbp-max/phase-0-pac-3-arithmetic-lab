5d53c12d0afb13036e595c8269db0518c03acabe
Mario Brito <mariobrito94@hotmail.com>
2022-02-12 17:52:58 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(){
+    
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    var c = a + b;
+    return c;
+}
+function subtract(a, b) {
+    var c = a - b;
+    return c;
+}
+function multiply(a, b) {
+    var c = a * b;
+    return c;
+}
+function divide(a, b) {
+    var c = a / b;
+    return c;
+}
+function increment(n) {
+    var c = ++n ;
+    return c;
+}
+function decrement(n){
+    var c = --n;
+    return c;
+}
+function makeInt(n) {
+    parseInt(n)
+}
+function makeInt(n) {
+    var c = parseInt(n);
+    return c;
+}
+function makeInt(n) {
+    var c = parseInt(n, 10);
+    return c;
+}
+function preserveDecimal(n) {
+    parseFloat(n)
+}
+function preserveDecimal(n) {
+    var c = parseFloat(n);
+    return c;
+}
